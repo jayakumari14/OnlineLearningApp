f19fde9a010274391dbfbd28803eb4fe72e39beb
jayak <kumarijaya127@gmail.com>
2024-11-22 15:44:55 +0530

    footer updated in herosection, dashboard ui updated

--- frontend/online-learning-frontend/src/components/HeroSection.js
+++ frontend/online-learning-frontend/src/components/HeroSection.js
@@ -31,7 +31,9 @@ const HeroSection = () => {
             src="https://images.unsplash.com/photo-1706804285677-4eb783c82642?q=80&w=1510&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" //
             alt="Learning"
           />
-          <p style={{ marginTop: "30px" }}>Designed by Jaya Kumari</p>
+          <p style={{ marginTop: "30px" }}>
+            Created by Jaya Kumari-kumarijaya127@gmail.com
+          </p>
         </div>
       </div>
     </>
